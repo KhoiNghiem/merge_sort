94f80b5c4078548579f32a779061b5c69ef87254
KhoiNghiem <khoinghiemtuan@gmail.com>
2025-03-12 02:33:55 +0800

    fix main

--- merge_sort_system.v
+++ merge_sort_system.v
@@ -1,164 +1,218 @@
+///////////////////////////////////////////////////////////////////////////////////////
 module merge_sort_system (
-    input clk,
-    input signed [7:0] In1, In2, In3, In4,
-    input BlkIn,
-    output reg [7:0] SortOut,
-    output reg OutValid
+input                        clk,
+input                        rst,
+
+input      signed [7:0]      In1, In2, In3, In4,
+output reg signed [7:0]      SortOut,
+output reg                   OutValid
+);
+wire                   BlkIn;
+wire                   group_done_tmp;
+wire signed [7:0]      max, second_max, min, second_min;
+
+reg [3:0] max_index_0, max_index_1, max_index_2, max_index_3;
+reg [3:0] max_index_4, max_index_5, max_index_6, max_index_7;
+
+reg  signed [7:0] block [0:7][0:3]; // Nơi lưu input data
+reg signed [7:0] block_buffer [0:7][0:3];
+
+reg         [2:0]      count; // Số không dấu để đếm từ 0 đến 7
+
+reg  group_done;
+
+sort4 sort_inst (
+.x1(In1), .x2(In2), .x3(In3), .x4(In4),
+.max(max), .second_max(second_max), .min(min), .second_min(second_min)
 );
 
-    reg signed [7:0] group [0:7][0:3];
-    reg signed [7:0] group_buffer [0:7][0:3];
-    reg [3:0] group_idx = 0;
 
-    reg [3:0] max_index_0 = 0, max_index_1 = 0, max_index_2 = 0, max_index_3 = 0;
-    reg [3:0] max_index_4 = 0, max_index_5 = 0, max_index_6 = 0, max_index_7 = 0;
-    wire signed [7:0] max, second_max, min, second_min;
+always @(negedge clk or negedge rst) begin
 
-    sort4 sort_inst (
-        .x1(In1), .x2(In2), .x3(In3), .x4(In4),
-        .max(max), .second_max(second_max), .min(min), .second_min(second_min)
-    );
+    if(~rst) begin
+        count <= 3'b000;
+    end
+    else begin 
+        count <= count + 1;
+    end
+end
 
-    reg [2:0] count = 0;
-    reg count_enabled = 0;
-    reg group_enabled = 0;
-
-    always @(posedge clk) begin
-        if (BlkIn) begin
-            group_enabled <= 1;
+always @(posedge clk or negedge rst) begin
+    if(~rst) begin
+        group_done <= 0;
+    end
+    else begin 
+        if (count + 1 >= 8) begin
+            group_done <= 1;
+        end else begin
+            
         end
+    end
+end
 
-        if (group_enabled) begin
-            if (group_idx < 8) begin
-                group[group_idx][0] <= max;
-                group[group_idx][1] <= second_max;
-                group[group_idx][2] <= second_min;
-                group[group_idx][3] <= min;
-                group_idx <= group_idx + 1;
-            end else begin
-                // Lưu vào group_buffer từng phần tử
-                group_buffer[0][0] <= group[0][0];
-                group_buffer[0][1] <= group[0][1];
-                group_buffer[0][2] <= group[0][2];
-                group_buffer[0][3] <= group[0][3];
+assign BlkIn = (!count); 
 
-                group_buffer[1][0] <= group[1][0];
-                group_buffer[1][1] <= group[1][1];
-                group_buffer[1][2] <= group[1][2];
-                group_buffer[1][3] <= group[1][3];
+always @(posedge clk or negedge rst) begin
+// Cập nhật block ở mỗi sườn âm clk vì In1-In4 cũng vậy
+    if(~rst) begin
+        block[0][0] <= 0; block[0][1] <= 0; block[0][2] <= 0; block[0][3] <= 0;
+        block[1][0] <= 0; block[1][1] <= 0; block[1][2] <= 0; block[1][3] <= 0;
+        block[2][0] <= 0; block[2][1] <= 0; block[2][2] <= 0; block[2][3] <= 0;
+        block[3][0] <= 0; block[3][1] <= 0; block[3][2] <= 0; block[3][3] <= 0;
+        block[4][0] <= 0; block[4][1] <= 0; block[4][2] <= 0; block[4][3] <= 0;
+        block[5][0] <= 0; block[5][1] <= 0; block[5][2] <= 0; block[5][3] <= 0;
+        block[6][0] <= 0; block[6][1] <= 0; block[6][2] <= 0; block[6][3] <= 0;
+        block[7][0] <= 0; block[7][1] <= 0; block[7][2] <= 0; block[7][3] <= 0;
+    end
 
-                group_buffer[2][0] <= group[2][0];
-                group_buffer[2][1] <= group[2][1];
-                group_buffer[2][2] <= group[2][2];
-                group_buffer[2][3] <= group[2][3];
+    else begin
+        block[count][0] <= max;
+        block[count][1] <= second_max;
+        block[count][2] <= second_min;
+        block[count][3] <= min;
+    end
+    end
 
-                group_buffer[3][0] <= group[3][0];
-                group_buffer[3][1] <= group[3][1];
-                group_buffer[3][2] <= group[3][2];
-                group_buffer[3][3] <= group[3][3];
+assign group_done_tmp = (count == 7);
 
-                group_buffer[4][0] <= group[4][0];
-                group_buffer[4][1] <= group[4][1];
-                group_buffer[4][2] <= group[4][2];
-                group_buffer[4][3] <= group[4][3];
+always @(negedge clk or negedge rst) begin
+    if (~rst) begin
+        
+    end
+    else if (group_done_tmp) begin
+        // Lưu vào block_buffer từng phần tử
+        block_buffer[0][0] <= block[0][0];
+        block_buffer[0][1] <= block[0][1];
+        block_buffer[0][2] <= block[0][2];
+        block_buffer[0][3] <= block[0][3];
 
-                group_buffer[5][0] <= group[5][0];
-                group_buffer[5][1] <= group[5][1];
-                group_buffer[5][2] <= group[5][2];
-                group_buffer[5][3] <= group[5][3];
+        block_buffer[1][0] <= block[1][0];
+        block_buffer[1][1] <= block[1][1];
+        block_buffer[1][2] <= block[1][2];
+        block_buffer[1][3] <= block[1][3];
 
-                group_buffer[6][0] <= group[6][0];
-                group_buffer[6][1] <= group[6][1];
-                group_buffer[6][2] <= group[6][2];
-                group_buffer[6][3] <= group[6][3];
+        block_buffer[2][0] <= block[2][0];
+        block_buffer[2][1] <= block[2][1];
+        block_buffer[2][2] <= block[2][2];
+        block_buffer[2][3] <= block[2][3];
 
-                group_buffer[7][0] <= group[7][0];
-                group_buffer[7][1] <= group[7][1];
-                group_buffer[7][2] <= group[7][2];
-                group_buffer[7][3] <= group[7][3];
+        block_buffer[3][0] <= block[3][0];
+        block_buffer[3][1] <= block[3][1];
+        block_buffer[3][2] <= block[3][2];
+        block_buffer[3][3] <= block[3][3];
 
-                count <= 0;
-                group_idx <= 0;
-                count_enabled <= 1;
-                OutValid <= 1'b1;
-            end
-        end
+        block_buffer[4][0] <= block[4][0];
+        block_buffer[4][1] <= block[4][1];
+        block_buffer[4][2] <= block[4][2];
+        block_buffer[4][3] <= block[4][3];
 
-        if (count_enabled) begin
-            if (count < 7) begin
-                // Tìm giá trị lớn nhất từ các phần tử
-                if (group_buffer[0][max_index_0] >= group_buffer[1][max_index_1] && 
-                    group_buffer[0][max_index_0] >= group_buffer[2][max_index_2] &&
-                    group_buffer[0][max_index_0] >= group_buffer[3][max_index_3] &&
-                    group_buffer[0][max_index_0] >= group_buffer[4][max_index_4] &&
-                    group_buffer[0][max_index_0] >= group_buffer[5][max_index_5] &&
-                    group_buffer[0][max_index_0] >= group_buffer[6][max_index_6] &&
-                    group_buffer[0][max_index_0] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[0][max_index_0];
-                    max_index_0 <= max_index_0 + 1;
-                end else if (group_buffer[1][max_index_1] >= group_buffer[0][max_index_0] && 
-                            group_buffer[1][max_index_1] >= group_buffer[2][max_index_2] &&
-                            group_buffer[1][max_index_1] >= group_buffer[3][max_index_3] &&
-                            group_buffer[1][max_index_1] >= group_buffer[4][max_index_4] &&
-                            group_buffer[1][max_index_1] >= group_buffer[5][max_index_5] &&
-                            group_buffer[1][max_index_1] >= group_buffer[6][max_index_6] &&
-                            group_buffer[1][max_index_1] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[1][max_index_1];
-                    max_index_1 <= max_index_1 + 1;
-                end else if (group_buffer[2][max_index_2] >= group_buffer[0][max_index_0] && 
-                            group_buffer[2][max_index_2] >= group_buffer[1][max_index_1] &&
-                            group_buffer[2][max_index_2] >= group_buffer[3][max_index_3] &&
-                            group_buffer[2][max_index_2] >= group_buffer[4][max_index_4] &&
-                            group_buffer[2][max_index_2] >= group_buffer[5][max_index_5] &&
-                            group_buffer[2][max_index_2] >= group_buffer[6][max_index_6] &&
-                            group_buffer[2][max_index_2] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[2][max_index_2];
-                    max_index_2 <= max_index_2 + 1;
-                end else if (group_buffer[3][max_index_3] >= group_buffer[0][max_index_0] && 
-                            group_buffer[3][max_index_3] >= group_buffer[1][max_index_1] &&
-                            group_buffer[3][max_index_3] >= group_buffer[2][max_index_2] &&
-                            group_buffer[3][max_index_3] >= group_buffer[4][max_index_4] &&
-                            group_buffer[3][max_index_3] >= group_buffer[5][max_index_5] &&
-                            group_buffer[3][max_index_3] >= group_buffer[6][max_index_6] &&
-                            group_buffer[3][max_index_3] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[3][max_index_3];
-                    max_index_3 <= max_index_3 + 1;
-                end else if (group_buffer[4][max_index_4] >= group_buffer[0][max_index_0] && 
-                            group_buffer[4][max_index_4] >= group_buffer[1][max_index_1] &&
-                            group_buffer[4][max_index_4] >= group_buffer[2][max_index_2] &&
-                            group_buffer[4][max_index_4] >= group_buffer[3][max_index_3] &&
-                            group_buffer[4][max_index_4] >= group_buffer[5][max_index_5] &&
-                            group_buffer[4][max_index_4] >= group_buffer[6][max_index_6] &&
-                            group_buffer[4][max_index_4] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[4][max_index_4];
-                    max_index_4 <= max_index_4 + 1;
-                end else if (group_buffer[5][max_index_5] >= group_buffer[0][max_index_0] && 
-                            group_buffer[5][max_index_5] >= group_buffer[1][max_index_1] &&
-                            group_buffer[5][max_index_5] >= group_buffer[2][max_index_2] &&
-                            group_buffer[5][max_index_5] >= group_buffer[3][max_index_3] &&
-                            group_buffer[5][max_index_5] >= group_buffer[4][max_index_4] &&
-                            group_buffer[5][max_index_5] >= group_buffer[6][max_index_6] &&
-                            group_buffer[5][max_index_5] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[5][max_index_5];
-                    max_index_5 <= max_index_5 + 1;
-                end else if (group_buffer[6][max_index_6] >= group_buffer[0][max_index_0] && 
-                            group_buffer[6][max_index_6] >= group_buffer[1][max_index_1] &&
-                            group_buffer[6][max_index_6] >= group_buffer[2][max_index_2] &&
-                            group_buffer[6][max_index_6] >= group_buffer[3][max_index_3] &&
-                            group_buffer[6][max_index_6] >= group_buffer[4][max_index_4] &&
-                            group_buffer[6][max_index_6] >= group_buffer[5][max_index_5] &&
-                            group_buffer[6][max_index_6] >= group_buffer[7][max_index_7]) begin
-                    SortOut <= group_buffer[6][max_index_6];
-                    max_index_6 <= max_index_6 + 1;
-                end else begin
-                    SortOut <= group_buffer[7][max_index_7];
-                    max_index_7 <= max_index_7 + 1;
-                end
+        block_buffer[5][0] <= block[5][0];
+        block_buffer[5][1] <= block[5][1];
+        block_buffer[5][2] <= block[5][2];
+        block_buffer[5][3] <= block[5][3];
 
-                count <= count + 1;
-            end else if (count == 7) begin
+        block_buffer[6][0] <= block[6][0];
+        block_buffer[6][1] <= block[6][1];
+        block_buffer[6][2] <= block[6][2];
+        block_buffer[6][3] <= block[6][3];
+
+        block_buffer[7][0] <= block[7][0];
+        block_buffer[7][1] <= block[7][1];
+        block_buffer[7][2] <= block[7][2];
+        block_buffer[7][3] <= block[7][3];
+    end
+    else begin
+        
+    end
+end
+
+always @(posedge clk or negedge rst) begin
+// Cập nhật OutValid ở mỗi sườn dương clk.
+if(~rst) begin 
+    max_index_0 <= 0;
+    max_index_1 <= 0;
+    max_index_2 <= 0;
+    max_index_3 <= 0;
+    max_index_4 <= 0;
+    max_index_5 <= 0;
+    max_index_6 <= 0;
+    max_index_7 <= 0;
+    SortOut <= 0;
+    OutValid <= 0;
+end
+else if (group_done) begin
+    OutValid <= 1'b1;
+    if (count - 1 <= 7) begin
+    // Tìm giá trị lớn nhất từ các phần tử
+    if (block_buffer[0][max_index_0] >= block_buffer[1][max_index_1] && 
+        block_buffer[0][max_index_0] >= block_buffer[2][max_index_2] &&
+        block_buffer[0][max_index_0] >= block_buffer[3][max_index_3] &&
+        block_buffer[0][max_index_0] >= block_buffer[4][max_index_4] &&
+        block_buffer[0][max_index_0] >= block_buffer[5][max_index_5] &&
+        block_buffer[0][max_index_0] >= block_buffer[6][max_index_6] &&
+        block_buffer[0][max_index_0] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[0][max_index_0];
+        max_index_0 <= max_index_0 + 1;
+    end else if (block_buffer[1][max_index_1] >= block_buffer[0][max_index_0] && 
+                block_buffer[1][max_index_1] >= block_buffer[2][max_index_2] &&
+                block_buffer[1][max_index_1] >= block_buffer[3][max_index_3] &&
+                block_buffer[1][max_index_1] >= block_buffer[4][max_index_4] &&
+                block_buffer[1][max_index_1] >= block_buffer[5][max_index_5] &&
+                block_buffer[1][max_index_1] >= block_buffer[6][max_index_6] &&
+                block_buffer[1][max_index_1] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[1][max_index_1];
+        max_index_1 <= max_index_1 + 1;
+    end else if (block_buffer[2][max_index_2] >= block_buffer[0][max_index_0] && 
+                block_buffer[2][max_index_2] >= block_buffer[1][max_index_1] &&
+                block_buffer[2][max_index_2] >= block_buffer[3][max_index_3] &&
+                block_buffer[2][max_index_2] >= block_buffer[4][max_index_4] &&
+                block_buffer[2][max_index_2] >= block_buffer[5][max_index_5] &&
+                block_buffer[2][max_index_2] >= block_buffer[6][max_index_6] &&
+                block_buffer[2][max_index_2] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[2][max_index_2];
+        max_index_2 <= max_index_2 + 1;
+    end else if (block_buffer[3][max_index_3] >= block_buffer[0][max_index_0] && 
+                block_buffer[3][max_index_3] >= block_buffer[1][max_index_1] &&
+                block_buffer[3][max_index_3] >= block_buffer[2][max_index_2] &&
+                block_buffer[3][max_index_3] >= block_buffer[4][max_index_4] &&
+                block_buffer[3][max_index_3] >= block_buffer[5][max_index_5] &&
+                block_buffer[3][max_index_3] >= block_buffer[6][max_index_6] &&
+                block_buffer[3][max_index_3] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[3][max_index_3];
+        max_index_3 <= max_index_3 + 1;
+    end else if (block_buffer[4][max_index_4] >= block_buffer[0][max_index_0] && 
+                block_buffer[4][max_index_4] >= block_buffer[1][max_index_1] &&
+                block_buffer[4][max_index_4] >= block_buffer[2][max_index_2] &&
+                block_buffer[4][max_index_4] >= block_buffer[3][max_index_3] &&
+                block_buffer[4][max_index_4] >= block_buffer[5][max_index_5] &&
+                block_buffer[4][max_index_4] >= block_buffer[6][max_index_6] &&
+                block_buffer[4][max_index_4] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[4][max_index_4];
+        max_index_4 <= max_index_4 + 1;
+    end else if (block_buffer[5][max_index_5] >= block_buffer[0][max_index_0] && 
+                block_buffer[5][max_index_5] >= block_buffer[1][max_index_1] &&
+                block_buffer[5][max_index_5] >= block_buffer[2][max_index_2] &&
+                block_buffer[5][max_index_5] >= block_buffer[3][max_index_3] &&
+                block_buffer[5][max_index_5] >= block_buffer[4][max_index_4] &&
+                block_buffer[5][max_index_5] >= block_buffer[6][max_index_6] &&
+                block_buffer[5][max_index_5] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[5][max_index_5];
+        max_index_5 <= max_index_5 + 1;
+    end else if (block_buffer[6][max_index_6] >= block_buffer[0][max_index_0] && 
+                block_buffer[6][max_index_6] >= block_buffer[1][max_index_1] &&
+                block_buffer[6][max_index_6] >= block_buffer[2][max_index_2] &&
+                block_buffer[6][max_index_6] >= block_buffer[3][max_index_3] &&
+                block_buffer[6][max_index_6] >= block_buffer[4][max_index_4] &&
+                block_buffer[6][max_index_6] >= block_buffer[5][max_index_5] &&
+                block_buffer[6][max_index_6] >= block_buffer[7][max_index_7]) begin
+        SortOut <= block_buffer[6][max_index_6];
+        max_index_6 <= max_index_6 + 1;
+    end else begin
+        SortOut <= block_buffer[7][max_index_7];
+        max_index_7 <= max_index_7 + 1;
+    end
+    end else if (count - 1 > 7) begin
                 SortOut <= 0;
-                count_enabled <= 0;
                 OutValid <= 1'b0;
                 max_index_0 <= 0;
                 max_index_1 <= 0;
@@ -168,7 +222,9 @@ module merge_sort_system (
                 max_index_5 <= 0;
                 max_index_6 <= 0;
                 max_index_7 <= 0;
-            end
-        end
-    end
+    end 
+end
+end
+
+
 endmodule
